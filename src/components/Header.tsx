efcd352c7195118bb1b6df920eebb9ea296fec02
skye-lopez <sky.lopez.dev@gmail.com>
2024-08-11 14:18:10 -0700

    Added better wireframe for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,16 @@ import {
     Text,
     Icon,
     Button,
+    Input,
+    InputGroup,
+    InputLeftAddon,
 } from "@chakra-ui/react";
+import {
+    SearchIcon,
+} from "@chakra-ui/icons";
 import { FaRegHeart } from "react-icons/fa";
 
+// TODO: LOTS, this is just a frame for now.
 export default function Header() {
     return (
         <Flex
@@ -15,7 +22,11 @@ export default function Header() {
             justifyContent="space-between"
             alignItems="center"
             padding="10px"
+            margin="10px 0px"
             marginBottom="20px"
+            background="white"
+            flexWrap="wrap"
+            borderRadius="10px"
         >
             {/* LOGO */}
             <Text
@@ -28,13 +39,47 @@ export default function Header() {
             >
                 TFT.WTF
             </Text>
+            {/* SEARCH */}
 
-            <Button
-                leftIcon={<Icon as={FaRegHeart} />}
-                colorScheme="pink"
+            {/* Buttons */}
+            <Flex
             >
-                Support Us
-            </Button>
+                <Flex
+                    margin="5px"
+                >
+                    <InputGroup>
+                        <InputLeftAddon>
+                            <SearchIcon />
+                        </InputLeftAddon>
+                        <Input
+                            width="100%"
+                        />
+                    </InputGroup>
+                </Flex>
+                <Button
+                    margin="5px"
+                >
+                    Units
+                </Button>
+                <Button
+                    margin="5px"
+                >
+                    Augments
+                </Button>
+                <Button
+                    colorScheme="purple"
+                    margin="5px"
+                >
+                    Teams
+                </Button>
+                <Button
+                    colorScheme="pink"
+                    margin="5px"
+                >
+                    <Icon as={FaRegHeart} />
+                </Button>
+            </Flex>
+
         </Flex>
     );
 }
